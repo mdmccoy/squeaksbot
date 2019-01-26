b39aaa916a46b3b59f48b31436c7cf3112f1ebbc
mdmccoy <mdmccoy@gmail.com>
2019-01-26 13:21:52 -0500

    More basic stuff added. Functionality more or less where I want it for now.

--- squeaksbot.rb
+++ squeaksbot.rb
@@ -3,12 +3,16 @@ require 'discordrb'
 
 # frozen_string_literal: true
 
-require 'discordrb'
-
 # bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 return puts "Need a token bub." unless ENV['DISCORD_TOKEN']
-bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_TOKEN'], prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_TOKEN'], prefix: '?'
 
+
+#########
+#
+# Constants. These will become instance variables when I make this a class.
+#
+#########
 SCHEDULE = {}
 DAYS = %w[Mon Tue Wed Thu Fri Sat Sun]
 HOURS = %w[1000 1100 1200 1300 1800 1900 2000]
@@ -16,8 +20,8 @@ ADMINS = %w[SqushiRex cmandr1 Squeaks]
 
 # Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
 # done once, afterwards, you can remove this part if you want
-puts "This bot's invite URL is #{bot.invite_url}."
-puts 'Click on it to invite it to your server.'
+# puts "This bot's invite URL is #{bot.invite_url}."
+# puts 'Click on it to invite it to your server.'
 
 
 #####################
@@ -47,7 +51,7 @@ def build_daily_schedule
 end
 
 def print_schedule(event, options = {})
-  event << "This weeks schedule -"
+  event << "Current schedule..."
   SCHEDULE.each do |day,times|
     event << "#{day}"
     times.each do |hour,slots|
@@ -61,7 +65,7 @@ def print_schedule(event, options = {})
   nil
 end
 
-def signup_instructions(event)
+def signup_help(event)
   event << days_help
   event << hours_help
   event << "To signup type '!signup <Day> <Hour>'."
@@ -94,15 +98,13 @@ bot.command(:hours) do |event|
 end
 
 bot.command(:instructions) do |event|
-  signup_instructions(event)
+  signup_help(event)
 end
 
 bot.command(:signup) do |event, *args|
   # Signs you up for the slot you asked for
   build_schedule
-
-  return 
-
+  
   day, hour = args
 
   return "Wrong Date Format, ex: Mon" unless DAYS.include? day
@@ -114,9 +116,9 @@ bot.command(:signup) do |event, *args|
   SCHEDULE[day][hour][0] == 'empty' ? SCHEDULE[day][hour][0] = event.user.name : SCHEDULE[day][hour][1]
 
   # Make this a PM?
-  "#{event.user.name} wants to sign up for #{day} at #{hour}"
+  event.user.pm "You have signed up for #{day} at #{hour}. !remove to remove."
 
-  print_schedule
+  print_schedule(event)
 end
 
 bot.command(:remove) do |event|
@@ -144,6 +146,14 @@ bot.command(:clear) do |event|
   print_schedule event
 
   nil
+end
+
+bot.command(:FUCK) do |event, fucker|
+  "Fuck you #{fucker}."
+end
+
+bot.command(:wake_up) do
+  "I'm awake."
 end
 
 # This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
